0e1fbd9b26eeab42b7ed7568f69f8d8e7b98b4c5
Roghashin Timbiti <rtimbiti@gmail.com>
2019-06-27 00:37:24 +0300

    Enable user to login

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,11 +1,15 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
+import LoginScreen from '../src/screens/Login';
 import HomeScreen from '../src/screens/Home';
 import ProfileScreen from '../src/screens/Profile';
 import WebViewScreen from '../src/screens/WebView/index';
 
 const AppNavigator = createStackNavigator(
   {
+    Login: {
+      screen: LoginScreen,
+    },
     Home: {
       screen: HomeScreen,
     },
@@ -17,7 +21,7 @@ const AppNavigator = createStackNavigator(
     },
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Login',
     defaultNavigationOptions: {
       headerStyle: {
         backgroundColor: '#F57F17',
